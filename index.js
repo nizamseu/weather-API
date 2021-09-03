5c04e268b34b32405ddefbf835962556741331be
nizamseu <nizamcse.seu@gmail.com>
2021-09-03 07:22:30 +0600

    icon url dynamically  set

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
+const API_key = "78fb9dae76bd04cfe5ba7aca206fa506";
 
+search = () => {
+  const inputField = document.getElementById("inputField");
+  const inputValue = inputField.value;
 
-const API_key ='78fb9dae76bd04cfe5ba7aca206fa506';
+  inputField.value = "";
 
-search =()=>{
-    const inputField = document.getElementById('inputField');
-    const inputValue =inputField.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_key}`;
 
-    inputField.value= ''
- 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_key}`
-   
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=> displayWeather(data))
-
-}
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayWeather(data));
+};
 
 // loadData = ()=> {
 
 //     const url = `https://api.openweathermap.org/data/2.5/weather?q=comilla&appid=${API_key}`
-   
+
 //     fetch(url)
 //     .then(res=>res.json())
 //     .then(data=> displayWeather(data))
@@ -27,17 +24,19 @@ search =()=>{
 // }
 // loadData();
 
-displayWeather =(data)=>{
-    const city = document.getElementById('city');
-    const tempp= document.getElementById('temp');
-    const tempType = document.getElementById('tempType');
-    
+displayWeather = (data) => {
+  const city = document.getElementById("city");
+  const tempp = document.getElementById("temp");
+  const tempType = document.getElementById("tempType");
+  const imgIcon = document.getElementById("icon");
 
-    const curruntTemp =(data?.main?.temp)-273.15;
-    const temp = curruntTemp.toFixed(2);
+  const curruntTemp = data?.main?.temp - 273.15;
+  const temp = curruntTemp.toFixed(2);
 
-    tempp.innerText = temp;
-    city.innerText = data.name;
-    tempType.innerText = data.weather[0].main
-    console.log(temp);
-}
+  tempp.innerText = temp;
+  city.innerText = data.name;
+  tempType.innerText = data.weather[0].main;
+  const url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  imgIcon.setAttribute("src", url);
+  console.log(temp);
+};
